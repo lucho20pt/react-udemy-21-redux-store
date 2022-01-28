1f36f49da248d591080fece3bb9571aea6f4b730
Daniel Batista <dani_batista20@hotmail.com>
2022-01-28 02:38:18 +0000

    add: dispatch useDispatch() and type of action Handlers()

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,19 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.scss";
 
 const Counter = () => {
   // uss
   const counter = useSelector((state) => state.counter);
   // usdf
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -12,6 +21,12 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>-- {counter} --</div>
+      <div className="row">
+        <button onClick={incrementHandler}>Increment</button>
+        <span> | </span>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <br />
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
