91c9609df3c8e023a23303e49e03f27ddd980e49
Daniel Batista <dani_batista20@hotmail.com>
2022-01-28 02:36:26 +0000

    add: useSelector() counter state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,17 @@
-import classes from './Counter.module.scss';
+import { useSelector } from "react-redux";
+import classes from "./Counter.module.scss";
 
 const Counter = () => {
+  // uss
+  const counter = useSelector((state) => state.counter);
+  // usdf
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>-- {counter} --</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
